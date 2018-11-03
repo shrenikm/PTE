6cbac23f4c1110cd91a080c494558b7aa4a73cd5
shrenikm <shrenik95@gmail.com>
2018-11-03 03:52:00 -0400

    Config directory.
    - Added a cartpole and color configuration struct mat file. Also added save files to generate them.
    - Added the readme for the directory.

---
+++ config/config_cartpole_save.m
@@ -0,0 +1,4 @@
+config_cartpole = struct;
+config_cartpole.length = 2;
+config_cartpole.mass = 1;
+save('config_cartpole.mat', 'config_cartpole');
---
+++ config/config_color_save.m
@@ -0,0 +1,7 @@
+config_color = struct;
+config_color.chrome = [0.9290, 0.6940, 0.1250];
+config_color.brown = [0.4, 0.3, 0.3];
+save('config_color.mat', 'config_color');
+
+
+    
