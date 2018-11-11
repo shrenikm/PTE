01c79518f18ac4bbbbbe683b4f0d5763b0a5afe1
shrenikm <shrenik95@gmail.com>
2018-11-10 20:23:38 -0500

    Fixed an error in the cost.

--- trajectory_optimization/direct_collocation/compute_cost.m
+++ trajectory_optimization/direct_collocation/compute_cost.m
@@ -6,6 +6,6 @@ g = 0;
 for i=1:N-1
     u_current = find_ui(i, z, n, p);
     u_next = find_ui(i+1, z, n, p);
-    g = g + 0.5*Dt*(u_current + u_next);
+    g = g + 0.5*Dt*(u_current^2 + u_next^2);
 end
 end
