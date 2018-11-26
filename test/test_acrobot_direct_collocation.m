e192efda30b1d6b3a99b0c565403f74484c25409
jaymilindanjankar <anjankar@seas.upenn.edu>
2018-11-26 16:38:35 -0500

    Avoiding merge conflicts

--- test/test_acrobot_direct_collocation.m
+++ test/test_acrobot_direct_collocation.m
@@ -1,3 +1,5 @@
+
+
 clear;
 clc;
 
@@ -16,24 +18,49 @@ daspect(ax, [1, 1, 1]);
 
 x0 = [1; 0; 0; 0];
 xf = [pi; 0; 0; 0];
-Dt = 0.1;
+x_star = xf;
+u_star = 0;
 
 N = 60;
+T = 12;
+Dt = T/N;
+u_lower = -30;
+u_upper = 30;
 
 n = numel(x0);
 p = 1; %Size of inputs
 assert(N-size(x0,1)-p>0);
-z_sol = direct_collocation_main(x0, xf, p, N, Dt, @dynamics_acrobot);
+
+Q = eye(n);
+R = eye(p);
+
+z_sol = direct_collocation_main(...
+	x0, xf, p, N, Dt, @dynamics_acrobot, u_lower, u_upper);
 
 fprintf('Initial state:\n');
 disp(z_sol(1:n));
 fprintf('Final state:\n');
 disp(z_sol(end-n-p+1:end-p));
 
-disp(z_sol);
 z_sol = reshape(z_sol, n+p, []);
 z_sol = z_sol(1:end-1, :);
-disp(z_sol);
+u_sol = z_sol(end, :);
+
+
+%simulate_trajectory_position(...
+ %   z_sol, linspace(0, (N-1)*Dt, N), @draw_acrobot, ax);
+
+[K, S] = lqr(A_acrobot(x_star, u_star), B_acrobot(x_star, u_star), ...
+    Q, R);
+threshold = 3;
+opts = odeset('MaxStep', 0.1,'RelTol',1e-4,'AbsTol',1e-4);
+
+[t_control_sol, x_control_sol] = ode45(@(t,x) control_dynamics_acrobot(...
+    t, x, u_sol, Dt, K, S, x_star, u_star, threshold), [0 Dt*(N)*1.5], x0, opts);
+
+% The output of ode45 gives the individual x values in a row.
+% We transpose as our plot assumes it to be placed column wise.
+x_control_sol = x_control_sol.';
 
 simulate_trajectory_position(...
-    z_sol, linspace(0, (N-1)*Dt, N), @draw_acrobot, ax);
+    x_control_sol, t_control_sol, @draw_acrobot, ax);
