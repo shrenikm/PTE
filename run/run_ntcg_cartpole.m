2db14375530c2c83842d7c99012e433b2b87be30
shrenikm <shrenik95@gmail.com>
2018-12-01 17:38:19 -0500

    Naive Trajectory Connectivity Graph initial commit.
    - Created the state space connectivity sparse matrix representation.
    
    - Created a run file to test the model.
    
    - The implementation needs to incorporate the control inputs next.

---
+++ run/run_ntcg_cartpole.m
@@ -0,0 +1,24 @@
+% Script to run the ntcg model on the cartpole
+
+clear;
+clc;
+
+% Adding the required paths
+addpath(genpath('../data/'));
+addpath(genpath('../dynamics/'));
+addpath(genpath('../environments/'));
+addpath(genpath('../integration/'));
+addpath(genpath('../models/'));
+addpath(genpath('../params/'));
+addpath(genpath('../trajectory_optimization/'));
+addpath(genpath('../tools/'));
+
+filepath = '';
+filename = 'cartpole_data_1.mat';
+load(strcat(filepath, filename));
+
+nx = size(x{1}, 1);
+nu = size(u{1}, 1);
+
+tg = generate_ntcg(x);
+
