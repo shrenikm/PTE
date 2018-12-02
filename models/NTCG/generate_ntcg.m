4743e82379bcab3f8cc7562e75599f336e9f6571
shrenikm <shrenik95@gmail.com>
2018-12-01 20:41:15 -0500

    Fixed all the errors in the naive graph generation.
    - Added an initial size to the sparse matrix to make the operations faster.
    
    - Fixed indexing errors.
    
    - Fixed the k index not being incremented to account for the skipping of the first trajectory node.

--- models/NTCG/generate_ntcg.m
+++ models/NTCG/generate_ntcg.m
@@ -5,22 +5,40 @@ function [tg, ug] = generate_ntcg(x_data, u_data)
     % Number of trajectories
     M = length(x_data);
     
+    % Obtaining the total number of nodes.
+    % (This wont be M*N if each trajectory has different number of knot
+    % points
+    N = zeros(M, 1);
+    for i=1:M
+        N(i) = size(x_data{i}, 2);     
+    end
+        
     % Sparse representation of the graph
-    tg = sparse(0);
+    tg = sparse(sum(N), sum(N));
     ug = [];
     k = 2;
     for i=1:M
         
-        N = size(x_data{i}, 2);
-        for j=2:N
+        for j=2:N(i)
             
-            tg(j-1, j) = 1;
-            tg(j, j-1) = -1;
+            % Filling up the tg graph
+            tg(k - 1, k) = 1;
+            tg(k, k - 1) = -1;
             
+            % Filling up the ug graph
             ug = [ug; [k-1, k, u_data{i}(:, j - 1)]];
+            
+            if j == N(i)
+                    
+            end
+            
             k = k + 1;
              
         end
+        
+        % Handling the face that we don't traverse the first node in any
+        % trajectory
+        k = k + 1;
         
     end
 
---
+++ models/NTCG/traverse_from_index.m
@@ -0,0 +1,7 @@
+% Function to traverse the graph from a given index
+
+function [] = traverse_from_index()
+
+    
+
+end
