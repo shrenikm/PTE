8b7c713a5987ee5023d013d84ea455ca43a868e7
jaymilindanjankar <anjankar@seas.upenn.edu>
2018-12-09 11:58:19 -0500

    Added PWS implementation of quadrotor. - Under the run/quadrotor/PWS folder, added PWS implementation of quadrotor. - Regenerated data for 50 trajectories for quadrotor to rectify previous bug by modifying create_quadrotor_data.m

---
+++ run/quadrotor/PWS/save_pws_state_quadrotor.m
@@ -0,0 +1,52 @@
+% Script to save the pws z state model on the quadrotor
+clear;
+clc;
+
+% Adding the required paths
+addpath(genpath('../../../data/'));
+addpath(genpath('../../../dynamics/'));
+addpath(genpath('../../../environments/'));
+addpath(genpath('../../../integration/'));
+addpath(genpath('../../../models/'));
+addpath(genpath('../../../params/'));
+addpath(genpath('../../../trajectory_optimization/'));
+addpath(genpath('../../../tools/'));
+addpath(genpath('../../../visualization/'));
+
+filepath = '';
+filename = 'quadrotor_data_1.mat';
+load(strcat(filepath, filename));
+
+nx = size(x{1}, 1);
+nu = size(u{1}, 1);
+
+% Variables in the lambda matrices which we need to compute through
+% optimization
+z0 = rand(nx*nx*N + nx*N, 1);
+
+%% Set variable for maxFunctionEvaluations
+iterations = 50000;
+
+% Options -------------------------------------------------------------
+options = optimoptions('fmincon', ...
+    'Display', 'iter', ...
+    'MaxFunctionEvaluations', iterations, ...
+    'StepTolerance', 1e-30, ...
+    'OptimalityTolerance', 1e-30);
+
+% -------------------------------------------------------------------------
+%% Part corresponds to computation of x_j
+problem.objective = @(z) compute_pws_objective(z, x, u, nx, N);
+problem.x0 = z0;
+problem.options = options;
+problem.solver = 'fmincon';
+
+disp('Solving');
+
+z_sol_state  = fmincon(problem);
+filepath = '../../../data/';
+filename_pws = 'pws_state_quadrotor_' + string(iterations) + '.mat';
+save(strcat(filepath, filename_pws), 'z_sol_state');
+
+% filename_pws = 'quadrotor_pws_50000.mat';
+% load(strcat(filepath , filename_pws));
